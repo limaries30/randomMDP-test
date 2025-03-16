e11bc71fdf8f1128328eb962110b02931b9887c4
limaries30 <limaries30@gmail.com>
2025-03-16 22:12:25 +0900

    TEST:pypi

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # randomMDP-test
+
+- This is a repo for making random Markov decision process (MDP).
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='test',
+    version='0.0.1',
+    description='This is test',
+    url='https://github.com/limaries30/randomMDP-test',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    license='MIT',
+    author='limaries30',
+    author_email='limaries30@kaist.ac.kr',
+)
